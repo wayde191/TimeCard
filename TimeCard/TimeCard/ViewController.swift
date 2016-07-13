f1e7e5900abf49ba54a17cd56410ece86a7f4c58
wayde191 <wwsun@thoughtworks.com>
2016-07-13 22:17:54 +0800

    Modified: add for uitableview and uiwebview; Wayde

--- TimeCard/TimeCard/ViewController.swift
+++ TimeCard/TimeCard/ViewController.swift
@@ -8,10 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIWebViewDelegate {
 
+    @IBOutlet weak var webview: UIWebView!
+    @IBOutlet weak var tableview: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        webview.loadRequest(NSURLRequest(URL: NSURL(string: "https://login.salesforce.com")!))
         
         print(NSDate.getTodayWeekStr())
         print(NSDate.get(.Next, "Sunday", considerToday:  true))
@@ -22,5 +27,61 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: UITableView DataSource and Delegate
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat{
+        return 55
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let identifier = "identtifier"
+        var cell = tableView.dequeueReusableCellWithIdentifier(identifier)
+        if(cell == nil){
+            cell = UITableViewCell(style: UITableViewCellStyle.Value1, reuseIdentifier: identifier)
+        }
+        
+        cell?.textLabel?.text = "TEXT"
+        cell?.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        return cell!
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    //MARK: UIWebView Delegate
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool{
+        let rurl =  request.URL?.absoluteString
+        print(rurl)
+        if (rurl!.hasPrefix("ios:")){
+            let method =  rurl!.componentsSeparatedByString("@")[1]
+            if method == "signin_go"{
+                signin_go()
+            }
+            return false
+        }
+        return true
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView){
+        //注入脚本,这个脚本把登录方法js重写为连接
+//        var jsPath =  NSBundle.mainBundle().pathForResource("app", ofType: "js")
+//        var jsContent:NSString = NSString (contentsOfFile: jsPath!, encoding: 0, error: nil)!
+//        jsContent=jsContent.stringByReplacingOccurrencesOfString("\n", withString: "")
+//        webView.stringByEvaluatingJavaScriptFromString(jsContent as String)
+    }
+    
+    func signin_go(){
+        NSLog("-我执行了signin_go-")
+    }
+    
 }
 
